be75d7e71fa82ece47b72898aa6befc54a9b7041
moserhu <moserhu@gmail.com>
2022-09-25 17:27:58 -0500

    Finish the UI and basic CSS

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
+//GAME STRUCTURE
 let playerScore = 0
 let computerScore = 0
 let roundWinner = ''  
 
 function playRound (playerSelection, computerSelection) {
-  
-
     if (playerSelection === computerSelection) {
         roundWinner = 'tie'
     }
@@ -39,43 +38,56 @@ function getRandomChoice() {
     }
   }
   
+function isGameOver() {
+    return playerScore === 5 || computerScore ===5
+}
 
+
+//UI
 
 const rock = document.querySelector(".rock")
 const paper = document.querySelector(".paper")
 const scissors = document.querySelector(".scissors")
-const newRound = document.querySelector(".newRound")
 const playerPrint = document.getElementById('playerPrint')
 const computerPrint = document.getElementById('computerPrint')
 const outcome = document.getElementById('outcome')
 const playerScoreboard = document.getElementById('playerScoreboard')
 const computerScoreboard = document.getElementById('computerScoreboard')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
 
-
-
-
-
+//Starting Parameters
+outcome.textContent = 'First to score 5 points wins the game'
+playerScoreboard.textContent = 'Player: 0'
+computerScoreboard.textContent = 'Computer: 0'
+//
 
 
 rock.addEventListener("click", () => handleClick('Rock'))
 paper.addEventListener("click", () => handleClick('Paper'))
 scissors.addEventListener("click", () => handleClick('Scissors'))
+restartBtn.addEventListener("click", restartGame)
+overlay.addEventListener('click', closeEndgameModal)
 
-
-newRound.addEventListener("click", function (e) {
-    playerPrint.textContent =""
-    computerPrint.textContent=""
-    outcome.textContent=''
-})
 
 function handleClick(playerSelection) {
+    if (isGameOver()) {
+        openEndgameModal()
+        return
+    }
+
     const computerSelection = getRandomChoice()
     playRound(playerSelection, computerSelection)
     makeChoices(playerSelection,computerSelection)
     updateScore()
-    
-}
 
+    if (isGameOver()) {
+        openEndgameModal()
+        setFinalMessage()
+    }
+}
 
 function makeChoices (playerSelection, computerSelection) {
     switch (playerSelection) {
@@ -90,7 +102,6 @@ function makeChoices (playerSelection, computerSelection) {
           break
     } 
 
-    
     switch (computerSelection) {
         case 'Rock':
             computerPrint.textContent = "Rock"
@@ -116,4 +127,36 @@ function updateScore() {
     playerScoreboard.textContent = `Player: ${playerScore}`
     computerScoreboard.textContent = `Computer: ${computerScore}`
   }
+
+function openEndgameModal() {
+    endgameModal.classList.add('active')
+    overlay.classList.add('active')
+  }
   
+function closeEndgameModal() {
+    endgameModal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+
+function setFinalMessage() {
+    return playerScore > computerScore
+      ? (endgameMsg.textContent = 'You won!')
+      : (endgameMsg.textContent = 'You lost...')
+  }
+  
+
+function restartGame() {
+playerScore = 0
+computerScore = 0
+outcome.textContent = 'First to score 5 points wins the game'
+playerPrint.textContent = ""
+computerPrint.textContent = ''
+playerScoreboard.textContent ='Player: 0'
+computerScoreboard.textContent = 'Computer: 0'
+endgameModal.classList.remove('active')
+overlay.classList.remove('active')
+}
+
+
+
+
